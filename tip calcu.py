35fd2213e7924109ce7223d33e56d6c102913eeb
Vikas6625 <vikasd662005@gmial.com>
2024-12-29 17:44:35 +0530

    tip calc indi

--- tip calcu.py
+++ tip calcu.py
@@ -2,5 +2,9 @@ print("welcome to tip calculator!")
 bill=float(input("what was the total bill? Rs."))
 tip=int(input("what percentage tip would you like to give? 10 12 15"))
 people=int(input("how many people to split the bill?"))
-people_with_tip=bill*(1+tip/100)
-print(people_with_tip)
+tip_as_percentage =tip/100
+total_tip_amount =bill*tip_as_percentage
+total_bill=bill+total_tip_amount 
+bill_per_person=total_bill/people
+final_amount=round(bill_per_person,2)
+print(f"each person should pay: Rs.{final_amount}")
