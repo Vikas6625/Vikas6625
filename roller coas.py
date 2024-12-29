af598e9cec5b4af0298e63883af3a06b82f37dd9
Vikas6625 <vikasd662005@gmial.com>
2024-12-29 22:01:37 +0530

    roller coaster

---
+++ roller coas.py
@@ -0,0 +1,8 @@
+print("welcome to the roller coaster")
+height = int(input("what is your height?"))
+
+if height >=120:
+    print("you can ride the roller coaster")
+else:
+    print("sorry you have to grow taller before you ride it.better lick next time")
+    
