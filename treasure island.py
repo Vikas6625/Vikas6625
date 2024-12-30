19cb10219ff2d9255e8303ef0f7b0b0e774e4734
Vikas6625 <vikasd662005@gmial.com>
2024-12-30 16:22:01 +0530

    treasure island

--- treasure island.py
+++ treasure island.py
@@ -1,18 +1,18 @@
 print("welcome to the treasure island! ")
 print ("your aim is to find the the treasure")
 choice1 = input('you\'re at a cross road,where do you wnt to go?'
-                'type "left" or "right".').lower()
+                'type "left" or "right".: ').lower()
 #there is a two way infront choose appropriate to find a way
 if choice1 == "left":
     choice2 = input('you\'ve come to lake.'
                     'there is an island in the middle of the lake.'
                     'type "wait" to wait for a boat.'
-                    'type "swim" to swim across.').lower()
+                    'type "swim" to swim across.: ').lower()
     if choice2 == "wait":
         choice3 = input("you arrived at a island unharmed."
                         "there is a house with 3 doors. one red,"
                         "one yellow and onle blue."
-                        "which colour do you choose?").lower()
+                        "which colour do you choose? :").lower()
         if choice3 == "red":
             print("it's a room of fire.game over")
         elif choice2 == "yellow":
